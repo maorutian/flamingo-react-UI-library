8a9e411f378935269def282e4576edc518723661
maoru <maorutian@gmail.com>
2020-08-05 10:23:27 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>This is h1</h1>
+                <h2>This is h2</h2>
+                <h3>This is h3</h3>
+                <h4>This is h4</h4>
+                <hr/>
+                <code>const a = b</code>
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
